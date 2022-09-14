f96f318a478f37eeb875a8ecdc4bc0937d2407e5
James-McAvilley <72215788+James-McAvilley@users.noreply.github.com>
2022-09-14 15:06:51 +0100

    JUnit test added

--- src/test/java/CucumberTests/FilmTest.java
+++ src/test/java/CucumberTests/FilmTest.java
@@ -2,18 +2,25 @@ package CucumberTests;
 
 import com.Sakila.api.SakilaApp.Film;
 import com.Sakila.api.SakilaApp.FilmRepository;
+import com.Sakila.api.SakilaApp.SakilaAppApplication;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class FilmTest {
     Film testFilm = new Film();
     FilmRepository filmRepository;
+    SakilaAppApplication sakilaAppApplication;
 
-    public FilmTest() {};
+    public FilmTest(Film testFilm, FilmRepository filmRepository, SakilaAppApplication sakilaAppApplication) {
+        this.testFilm = testFilm;
+        this.filmRepository = filmRepository;
+        this.sakilaAppApplication = sakilaAppApplication;
+    }
 
     @Test
     void GetFilm(){

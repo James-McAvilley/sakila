8424dae06d56fa115524645af6bdc477cfd967e5
James-McAvilley <72215788+James-McAvilley@users.noreply.github.com>
2022-09-15 10:32:10 +0100

    Application test added

--- src/test/java/CucumberTests/SakilaAppTest.java
+++ src/test/java/CucumberTests/SakilaAppTest.java
@@ -1,24 +1,29 @@
 package CucumberTests;
 
-import com.Sakila.api.SakilaApp.*;
+import com.Sakila.api.SakilaApp.ActorRepository;
+import com.Sakila.api.SakilaApp.CategoryRepository;
+import com.Sakila.api.SakilaApp.FilmRepository;
+import com.Sakila.api.SakilaApp.SakilaAppApplication;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 public class SakilaAppTest {
-    ActorRepository testActorRepo;
-    FilmRepository testFilmRepo;
-    CategoryRepository testCategoryRepo;
+    private ActorRepository actorRepository;
+    private FilmRepository filmRepository;
+    private CategoryRepository categoryRepository;
+    SakilaAppApplication sakilaAppApplication;
 
-    public SakilaAppTest(ActorRepository testActorRepo, FilmRepository testFilmRepo, CategoryRepository testCategoryRepo) {
-        this.testActorRepo = testActorRepo;
-        this.testFilmRepo = testFilmRepo;
-        this.testCategoryRepo = testCategoryRepo;
+    public SakilaAppTest(){
+        actorRepository = mock(ActorRepository.class);
+        filmRepository = mock(FilmRepository.class);
+        categoryRepository = mock(CategoryRepository.class);
+        sakilaAppApplication = new SakilaAppApplication(actorRepository, filmRepository, categoryRepository);
     }
 
     @Test
     void GetAllActors() {
-        assertNotNull(testActorRepo.findAll());
+        assertNotNull(actorRepository.findAll());
     }
 }
